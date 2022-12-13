b945eb3718f3617740d74627984386eed68a4439
Aniel Feyt <aniel@mainboard.com>
2022-12-13 09:00:17 +0200

    Updated header

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,9 +25,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Aniel's Website Test</h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
